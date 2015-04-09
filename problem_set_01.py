6c0c1f42f9f742c11355ed289738f4fc9945b27c
Gabriel Sobral <gasan.sobral@gmail.com>
2015-04-09 00:02:03 -0300

    Register new quadrilaterals and include license plate string

--- problem_set_01.py
+++ problem_set_01.py
@@ -7,15 +7,15 @@ original_img = None
 quadrilaterals = []
 selected_vert = None
 selected_quad = None
+registering_quad = 0
+new_quad = {'x': [], 'y': []}
 
 
 class Quadrilateral:
-    x = []
-    y = []
-
-    def __init__(self, x, y):
+    def __init__(self, x, y, plate):
         self.x = x
         self.y = y
+        self.plate = plate
 
     def contains(self, x, y):
         if x > min(self.x) and x < max(self.x) and y > min(self.y) and y < max(self.y):
@@ -24,7 +24,6 @@ class Quadrilateral:
 
     def has_vertice(self, x, y):
         for i in range(0, 4):
-            print i, self.x[i], self.y[i]
             if x >= (self.x[i] - 10) and x <= (self.x[i] + 10) and y >= (self.y[i] - 10) and y <= (self.y[i] + 10):
                 return i
         return -1
@@ -42,6 +41,7 @@ class Quadrilateral:
             self.y[i] += dy
 
     def draw(self, img):
+        self.print_points()
         # draw lines
         for i in range(-1, 3):
             cv2.line(img,
@@ -56,10 +56,19 @@ class Quadrilateral:
                           (self.x[i] + 10, self.y[i] + 10),
                           (255, 0, 255),
                           -1)
+        # draw license plate string
+        cv2.putText(img, self.plate, (self.x[1], self.y[1]), cv2.FONT_HERSHEY_SIMPLEX, 2,
+      (255,0,255), 5)
 
     def print_points(self):
         print 'x:', self.x
         print 'y:', self.y
+
+    def set_plate(self, plate):
+        self.plate = plate
+
+    def print_plate(self):
+        print self.plate
 
 
 def on_mouse_event(event, x, y, flags, param):
@@ -67,18 +76,29 @@ def on_mouse_event(event, x, y, flags, param):
     global selected_vert
     global selected_quad
     global original_img
+    global registering_quad
+    global new_quad
 
     if event == cv2.EVENT_LBUTTONDOWN:
         print 'I: left button down:', x, y
+
+        if registering_quad:
+            new_quad['x'].append(x)
+            new_quad['y'].append(y)
+            registering_quad -= 1
+            if registering_quad == 0:
+                new_quadrilateral(new_quad['x'], new_quad['y'])
+            return
+
         for quad in quadrilaterals:
             i = quad.has_vertice(x, y)
             if i != -1:
                 selected_vert = {'quad': quad, 'vert': i}
-                print 'I: selected vertice:', selected_vert
+                print 'I: selected vertice:', i
                 break
             elif quad.contains(x, y):
                 selected_quad = {'quad': quad, 'x0': x, 'y0': y}
-                print 'I: selected quad:', selected_quad
+                print 'I: selected quad'
                 break
     elif event == cv2.EVENT_LBUTTONUP:
         print 'I: left button up:', x, y
@@ -88,8 +108,6 @@ def on_mouse_event(event, x, y, flags, param):
         elif selected_quad:
             quad = selected_quad['quad']
             quad.move(x - selected_quad['x0'], y - selected_quad['y0'])
-        else:
-            return
 
         selected_vert = None
         selected_quad = None
@@ -99,20 +117,34 @@ def on_mouse_event(event, x, y, flags, param):
             quad.draw(img)
         cv2.imshow(window, img)
 
+def new_quadrilateral(x, y):
+    global quadrilaterals
+
+    print 'I: new quadrilateral:', x, y
+
+    q = Quadrilateral(x, y, 'abc1234')
+    quadrilaterals.append(q)
+
+
 cv2.namedWindow(window, cv2.WINDOW_NORMAL)
 cv2.setMouseCallback(window, on_mouse_event)
 
 original_img = cv2.imread('../dataset_01/L0.jpg', cv2.IMREAD_UNCHANGED)
 
-q = Quadrilateral([100, 150, 400, 500], [300, 200, 300, 800])
+q = Quadrilateral([100, 150, 400, 500], [300, 200, 300, 800], 'abc1234')
 quadrilaterals.append(q)
 
 img = original_img.copy()
 for quad in quadrilaterals:
-    quad.print_points()
     quad.draw(img)
 cv2.imshow(window, img)
 
-k = cv2.waitKey(0)
-if k == 27:         # wait for ESC key to exit
-    cv2.destroyAllWindows()
+while True:
+    k = cv2.waitKey(10)
+    if k == ord('n'):
+        print 'I: registering new quadrilateral'
+        registering_quad = 4
+    elif k == 27:         # wait for ESC key to exit
+        break
+
+cv2.destroyAllWindows()
