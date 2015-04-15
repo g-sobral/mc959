c4a31ee92361637ee23b947fff7dd9d28e5e7348
Gabriel Sobral <gabriel.sobral@eldorado.org.br>
2015-04-15 14:39:32 -0300

    Write questions. Organize code.

--- problem_set_02/ps02.py
+++ problem_set_02/ps02.py
@@ -1,44 +1,33 @@
 #!/usr/bin/python2.7
 # -*- coding: utf-8 -*-
 
-"""problem_set_02
+"""Problem Set 02 - Clustering (GRP, IS)
+
+Python, OpenCV and K-Means
+
+In this assignment, use python and OpenCV. Grab the dataset (file digits.raw)
+from the assignment page on the course Moodle. Each line of the file is a data
+element – 64 integers ([0-16] range) separated by commas that represents an
+8x8 matrix, or image snippet, of a manuscript digit.
+
+Author: Gabriel Sobral <gasan.sobral@gmail.com>
 
 """
 
 import numpy as np
 import cv2
-import sys
 
 digits = []
 
-
-def draw_digit(digit, k, lines=False):
-    pixel_size = k/8
-    image = np.empty((k,k), dtype=np.uint8)
-    for i in range(0, k, pixel_size):
-        for j in range(0, k, pixel_size):
-            pixel_value = digit.pop(0) * 255/16
-            for x in range(0, pixel_size):
-                for y in range(0, pixel_size):
-                    if lines and (x == 0 or y == 0):
-                        image[i+x, j+y] = 255
-                    else:
-                        image[i+x, j+y] = pixel_value
-
-    cv2.namedWindow('window', cv2.WINDOW_NORMAL)
-    cv2.imshow('window', image)
-    cv2.imwrite('teste.png', image)
-    cv2.waitKey(0)
+#
+# Q1: Using python and OpenCV, learn how to read the data.
+# Improvement: read to np array instead of list
+#
 
 try:
-    file_path = sys.argv[1]
-except IndexError:
-    print 'Usage: ps02 FILE_PATH'
-    exit()
-
-try:
-    f = open(file_path, 'r')
-    print 'reading file:', file_path
+    filepath = 'digits.raw'
+    f = open('digits.raw', 'r')
+    print 'reading file:', filepath
     for line in f:
         line = line.rstrip('\n')
         line = line.split(',')
@@ -46,25 +35,71 @@ try:
         digits.append(line)
     f.close()
 except IOError:
-    print 'file not found'
+    print 'file not found:', filepath
     exit()
 
 print 'number of digits:', len(digits)
 print 'elements per digit:', len(digits[0])
 
-# draw_digit(digits[0], 344, True)
+#
+# Q2: Write a function that receives to parameters, a data element and a
+#     size k, which returns an image with resolution kxk that represent
+#     the grayscale digit.
+#
 
-print len(digits)
+
+def draw_digit(digit, k, lines=False):
+    pixel_size = k/8
+    image = np.empty((k, k), dtype=np.uint8)
+    for i in range(0, k, pixel_size):
+        for j in range(0, k, pixel_size):
+            pixel_value = digit.pop(0) * 255/16
+            for x in range(0, pixel_size):
+                for y in range(0, pixel_size):
+                    if lines and (x == 0 or y == 0):
+                        image[i+x, j+y] = 0
+                    else:
+                        image[i+x, j+y] = pixel_value
+
+    cv2.namedWindow('window', cv2.WINDOW_NORMAL)
+    cv2.imshow('window', image)
+    # cv2.imwrite('teste.png', image)
+    cv2.waitKey(0)
+
+draw_digit(list(digits[0]), 344, True)
+
+#
+# Q3: Use OpenCV’s KMeans implementation to explore and cluster the dataset
+#     in 10 groups.
+#
+
 data = np.array(digits)
-print data.shape
-print data
 data = np.float32(data)
 
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 flags = cv2.KMEANS_RANDOM_CENTERS
+compactness, labels, centers = cv2.kmeans(data, 10, criteria, 10, flags)
 
-retval, bestLabels, centers = cv2.kmeans(data, 10, criteria, 10, flags)
+#
+# Q3a: Use the function of Question 2 to draw the centroid of every cluster.
+#
 
-print 'retval:', retval
-print 'bestLabels:', bestLabels
-print 'centers:', centers
+for center in centers:
+    draw_digit(center.tolist(), 200, True)
+
+#
+# Q3b: Analyze the algorithm’s sensitivity according to changes in the
+#      initial seeds.
+#
+
+
+#
+# Q4: Calculate the Covariance Matrix of each of the groups.
+#
+
+
+#
+# Q5: For each group, calculate the Mahalanobis distance of every element
+#     to the centroid, then draw the centroid followed by the three farthest
+#     elements (showing their Mahalanobis distance).
+#
