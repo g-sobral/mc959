0f34213fdaca1d1c09ae5dd0218a844fab2daa36
Gabriel Sobral <gabriel.sobral@eldorado.org.br>
2015-04-15 12:26:58 -0300

    Improve draw_digit() to support different resolutions

--- problem_set_02/ps02.py
+++ problem_set_02/ps02.py
@@ -12,14 +12,22 @@ import sys
 digits = []
 
 
-def draw_digit(digit, pixel_size):
-    image = np.empty((8,8), dtype=np.uint8)
-    for i in range(0, 8):
-        for j in range(0, 8):
-            image[i, j] = digit.pop(0) * 255/16
+def draw_digit(digit, k, lines=False):
+    pixel_size = k/8
+    image = np.empty((k,k), dtype=np.uint8)
+    for i in range(0, k, pixel_size):
+        for j in range(0, k, pixel_size):
+            pixel_value = digit.pop(0) * 255/16
+            for x in range(0, pixel_size):
+                for y in range(0, pixel_size):
+                    if lines and (x == 0 or y == 0):
+                        image[i+x, j+y] = 255
+                    else:
+                        image[i+x, j+y] = pixel_value
 
     cv2.namedWindow('window', cv2.WINDOW_NORMAL)
     cv2.imshow('window', image)
+    cv2.imwrite('teste.png', image)
     cv2.waitKey(0)
 
 try:
@@ -44,4 +52,4 @@ except IOError:
 print 'number of digits:', len(digits)
 print 'elements per digit:', len(digits[0])
 
-draw_digit(digits[0], 1)
+draw_digit(digits[0], 344, True)
