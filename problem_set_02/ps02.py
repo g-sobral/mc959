b45c87e49cbaeb156e9bc90e5026b585d43c8489
Gabriel Sobral <gabriel.sobral@eldorado.org.br>
2015-04-15 12:17:25 -0300

    Implement draw_digit

--- problem_set_02/ps02.py
+++ problem_set_02/ps02.py
@@ -11,6 +11,17 @@ import sys
 
 digits = []
 
+
+def draw_digit(digit, pixel_size):
+    image = np.empty((8,8), dtype=np.uint8)
+    for i in range(0, 8):
+        for j in range(0, 8):
+            image[i, j] = digit.pop(0) * 255/16
+
+    cv2.namedWindow('window', cv2.WINDOW_NORMAL)
+    cv2.imshow('window', image)
+    cv2.waitKey(0)
+
 try:
     file_path = sys.argv[1]
 except IndexError:
@@ -32,3 +43,5 @@ except IOError:
 
 print 'number of digits:', len(digits)
 print 'elements per digit:', len(digits[0])
+
+draw_digit(digits[0], 1)
