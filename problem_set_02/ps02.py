Fix mahalanobis distance computation. Remove .png files

problem_set_02/ps02.py
@@ -20,13 +20,12 @@ import cv2
 
 #
 # Q1: Using python and OpenCV, learn how to read the data.
-# Improvement: read to np array instead of list
 #
 
 digits = []
 try:
     filepath = 'digits.raw'
-    f = open('digits.raw', 'r')
+    f = open(filepath, 'r')
     print 'reading file:', filepath
     for line in f:
         line = line.rstrip('\n')
@@ -62,7 +61,6 @@ def draw_digit(digit, k, lines=False, save=False, image_name='digit.png'):
                         image[i+x, j+y] = 0
                     else:
                         image[i+x, j+y] = pixel_value
-
     if save:
         cv2.imwrite(image_name, image)
         print 'saving:', image_name
@@ -88,9 +86,9 @@ compactness, labels, centroids = cv2.kmeans(data, 10, criteria, 10, flags)
 # Q3a: Use the function of Question 2 to draw the centroid of every cluster.
 #
 
-for c in range(0, len(centroids)):
-    image_name = 'centroid' + str(c) + '.png'
-    draw_digit(centroids[c].tolist(), 200, True, True, image_name)
+for index, centroid in enumerate(centroids):
+    image_name = 'centroid' + str(index) + '.png'
+    draw_digit(centroid.tolist(), 200, True, True, image_name)
 
 #
 # Q3b: Analyze the algorithm’s sensitivity according to changes in the
@@ -114,13 +112,9 @@ for c in range(0, 10):
     print 'group:', c, len(grouped_data[c])
     x = np.array(grouped_data[c])
     covar, mean = cv2.calcCovarMatrix(x, flags)
-    # print 'covar:', covar.shape
-    retval, icovar = cv2.invert(covar)
-    # print 'icovar:', icovar.shape
+    icovar = np.empty(covar.shape, covar.dtype)
+    cv2.invert(covar, icovar, cv2.DECOMP_SVD)
     icovariance.append(icovar)
-
-# cv2.calcCovarMatrix(samples, flags[, covar[, mean[, ctype]]]) → covar, mean
-# cv2.invert(src[, dst[, flags]]) → retval, dst
 
 
 #
@@ -128,11 +122,17 @@ for c in range(0, 10):
 #     to the centroid, then draw the centroid followed by the three farthest
 #     elements (showing their Mahalanobis distance).
 #
-# FIXME: fix argument types for mahalanobis algorithm
-#
 
-print 'centroid:', centroids[0].type(), centroids[0].size()
-print 'point:', grouped_data[0][0].type(), grouped_data[0][0].size()
-print 'icovarinace:', icovariance[0].type(), icovariance[0].rows, icovariance[0].cols
+for index, centroid in enumerate(centroids):
+    points = np.float32(grouped_data[index])
+    icovar = np.float32(icovariance[index])
 
-mdistance = cv2.Mahalanobis(centroids[0], grouped_data[0][0], icovariance[0])
+    mdistance = []
+    for point in points:
+        mdistance.append(cv2.Mahalanobis(centroid, point, icovar))
+
+    farthest = sorted(mdistance, reverse=True)[:3]
+    for far in farthest:
+        print mdistance.index(far), far
+        image_name = 'g' + str(index) + '_dist' + str(far) +  '.png'
+        draw_digit(points[mdistance.index(far)].tolist(), 200, True, True, image_name)